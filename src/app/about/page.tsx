7d9dc73a1a8a86bdfc2457376d9330cce010319e
Saputra <saputra.uta50@gmail.com>
2024-07-12 20:49:27 +0800

    static metadata

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React from "react";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "About Page By Next.js",
+  description: "About",
+};
 
 export default function page() {
-  return (
-    <div>About page</div>
-  )
+  return <div>About page</div>;
 }
